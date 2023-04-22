e95a673239b235c8827becfa84cd225ee2a39a61
Ayush20200012016 <ayush2020012016@tulas.edu.in>
2023-04-22 18:03:40 +0530

    added the homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import Login from "./Components/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Login />}></Route>
+        <Route
+          exact
+          path="/home"
+          element={
+            <>
+              <Header />
+              <Home />
+            </>
+          }
+        ></Route>
+      </Routes>
+    </Router>
+  );
+};
 
 export default App
---
+++ src/Components/Home.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
